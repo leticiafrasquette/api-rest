b8990db5cef754c843d90efe1f24e65d0be87aba
Letícia Frasquette <frasquetteleticia@gmail.com>
2022-08-24 14:37:01 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+    'import/first': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+  },
+};
---
+++ src/database/seeds/20220824114316-criar-usuarios.js
@@ -0,0 +1,36 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'Pessoa 1',
+          email: 'pessoa1@teste.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Pessoa 2',
+          email: 'pessoa2@teste.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Pessoa 3',
+          email: 'pessoa3@teste.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  // eslint-disable-next-line no-empty-function
+  async down() {},
+};
